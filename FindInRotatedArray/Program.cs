34b8b208c80fb95415ad446584d64b4dfba558ed
Tugberk Ugurlu <tugberk@outlook.com>
2017-11-11 13:05:02 +0000

    brute force solution

---
+++ FindInRotatedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FindInRotatedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = new [] {5, 6, 7, 8, 9, 10, 1, 2, 3};
+            var key = 3;
+            
+            Console.WriteLine(FindIndexOfAnElement(inputArray, key));
+        }
+
+        /* 
+            Search an element in a sorted and rotated array
+
+            Input  : arr[] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+                    key = 3
+            Output : Found at index 8
+        */
+        private static int FindIndexOfAnElement(int[] array, int key) 
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == key)
+                {
+                    return i;
+                }
+            }
+
+            throw new NotSupportedException();
+        } 
+    }
+}
