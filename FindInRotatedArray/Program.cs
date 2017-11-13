45fd07bba4af177a86e18ab2a0100a0aa9ae119f
Tugberk Ugurlu <tugberk@outlook.com>
2017-11-13 21:46:24 +0000

    better

--- FindInRotatedArray/Program.cs
+++ FindInRotatedArray/Program.cs
@@ -7,9 +7,9 @@ namespace FindInRotatedArray
         static void Main(string[] args)
         {
             var inputArray = new [] {5, 6, 7, 8, 9, 10, 1, 2, 3};
-            var key = 3;
-            
-            Console.WriteLine(FindIndexOfAnElement(inputArray, key));
+            var key = 6;
+
+            Console.WriteLine(FindIndexOfAnElementBetter(inputArray, key));
         }
 
         /* 
@@ -18,7 +18,10 @@ namespace FindInRotatedArray
             Input  : arr[] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
                     key = 3
             Output : Found at index 8
+
+            https://articles.leetcode.com/searching-element-in-rotated-array/
         */
+
         private static int FindIndexOfAnElement(int[] array, int key) 
         {
             for (int i = 0; i < array.Length; i++)
@@ -31,5 +34,26 @@ namespace FindInRotatedArray
 
             throw new NotSupportedException();
         } 
+
+        private static int FindIndexOfAnElementBetter(int[] array, int key) 
+        {
+            int leftMost = 0;
+            int rightMost = array.Length - 1;
+
+            while (array[leftMost] > array[rightMost])
+            {
+                var midPoint = leftMost + (rightMost - leftMost) / 2;
+                if(array[midPoint] > array[rightMost]) 
+                {
+                    leftMost = midPoint + 1;
+                }
+                else 
+                {
+                    rightMost = midPoint;
+                }
+            }
+
+            return leftMost;
+        }
     }
 }
